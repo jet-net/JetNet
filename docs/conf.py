cba3adf1a8d35e9abf9063bbe83c6f180845221a
rkansal47 <raghavk1997@yahoo.co.in>
2021-09-17 15:17:26 -0700

    pin version in requirements.tt and mock import energyflow

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
-
+autodoc_mock_imports = ['energyflow']
 
 # -- Project information -----------------------------------------------------
 
