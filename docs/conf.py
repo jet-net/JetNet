1ea79b35184bc866ee2c52a72427bf7a1537e905
rkansal47 <raghavk1997@yahoo.co.in>
2021-09-18 08:53:59 -0700

    update gitignore

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../'))
 autodoc_mock_imports = ['energyflow', 'awkward', 'coffea']
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +22,7 @@ copyright = '2021, Raghav Kansal'
 author = 'Raghav Kansal'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.1b'
 
 
 # -- General configuration ---------------------------------------------------
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='jetnet',
+      version='0.0.1b',
+      description='One jet library to rule them all.',
+      url='http://github.com/rkansal47/JetNet',
+      author='Raghav Kansal',
+      author_email='rkansal@cern.ch',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
