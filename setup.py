96c3b8ae4647702997b68c079ea430b7fe926d4c
rkansal47 <raghavk1997@yahoo.co.in>
2022-12-05 21:20:24 +0100

    black

--- setup.py
+++ setup.py
@@ -18,6 +18,26 @@ install_requires = [
 extras_require = {"emdloss": ["qpth", "cvxpy"]}
 
 
+classifiers = (
+    [
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
+
+
 def readme():
     with open("README.md") as f:
         return f.read()
@@ -39,7 +59,9 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=install_requires,
+    python_requires=">=3.7",
     extras_require=extras_require,
+    classifiers=classifiers,
     zip_safe=False,
     include_package_data=True,
 )
