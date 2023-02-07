57209515a23249737da07bc10d18675686fead7e
cpareja3025 <cpareja3025@gmail.com>
2023-02-07 03:45:49 -0800

    adding JetClass along with its utils file

--- jetnet/datasets/JetClass.py
+++ jetnet/datasets/JetClass.py
@@ -2,51 +2,37 @@ from typing import Callable, List, Set, Union, Optional, Tuple
 import numpy as np
 import logging
 import uproot
+import os
 from utils import *
-
-
 
 class JetClass:
     """
-    PyTorch ``torch.unit.data.Dataset`` class for the JetNet dataset.
-    If hdf5 files are not found in the ``data_dir`` directory then dataset will be downloaded
-    from Zenodo (https://zenodo.org/record/6975118 or https://zenodo.org/record/6975117).
+    PyTorch ``torch.unit.data.Dataset`` class for the JetClass dataset.
+    If root files are not found in the ``data_dir`` directory then dataset will be downloaded
+    from Zenodo (https://zenodo.org/record/6619768).
     Args:
-        jet_type (Union[str, Set[str]], optional): individual type or set of types out of
-            'g' (gluon), 'q' (light quarks), 't' (top quarks), 'w' (W bosons), or 'z' (Z bosons).
-            "all" will get all types. Defaults to "all".
+        jet_type (Union[str, Set[str]], optional): individual type or set of types out of 'HToBB' ,
+            "HtoCC", "HtoGG", "HtoWW", "HtoWW2Q1L", "HtoWW4Q", "TTBar", "TTBarLep", "WtoQQ",
+             "ZJetstoNuNu", "ZtoQQ" ). "all" will get all types. Defaults to "all".
         data_dir (str, optional): directory in which data is (to be) stored. Defaults to "./".
         particle_features (List[str], optional): list of particle features to retrieve. If empty
             or None, gets no particle features. Defaults to
-            ``["etarel", "phirel", "ptrel", "mask"]``.
+            `` ["part_px", "part_py", "part_pz", "part_energy", "part_deta", "part_dphi", "part_d0val", 
+            "part_d0err", "part_dzval", "part_dzerr", "part_charge", "part_isChargedHadron", 
+            "part_isNeutralHadron", "part_isPhoton", "part_isElectron", "part_isMuon"]``.
         jet_features (List[str], optional): list of jet features to retrieve.  If empty or None,
             gets no jet features. Defaults to
-            ``["type", "pt", "eta", "mass", "num_particles"]``.
-        particle_normalisation (NormaliseABC, optional): optional normalisation to apply to
-            particle data. Defaults to None.
-        jet_normalisation (NormaliseABC, optional): optional normalisation to apply to jet data.
-            Defaults to None.
-        particle_transform (callable, optional): A function/transform that takes in the particle
-            data tensor and transforms it. Defaults to None.
-        jet_transform (callable, optional): A function/transform that takes in the jet
-            data tensor and transforms it. Defaults to None.
-        num_particles (int, optional): number of particles to retain per jet, max of 150.
-            Defaults to 30.
-        split (str, optional): dataset split, out of {"train", "valid", "test", "all"}. Defaults
-            to "train".
-        split_fraction (List[float], optional): splitting fraction of training, validation,
-            testing data respectively. Defaults to [0.7, 0.15, 0.15].
-        seed (int, optional): PyTorch manual seed - important to use the same seed for all
-            dataset splittings. Defaults to 42.
+            ``["jet_pt", "jet_eta", "jet_phi", "jet_energy", "jet_nparticles", "jet_sdmass", "jet_tau1",
+               "jet_tau2", "jet_tau3", "jet_tau4"]``.
     """
 
-    _zenodo_record_ids = {"30": 6975118, "150": 6975117}
+    zenodo_record_id = 6619768
 
-    max_num_particles = 150
-
-    jet_types = ["g", "q", "t", "w", "z"]
-    all_particle_features = ["etarel", "phirel", "ptrel", "mask"]
-    all_jet_features = ["type", "pt", "eta", "mass", "num_particles"]
+    jet_type = ["HtoBB", "HtoCC", "HtoGG", "HtoWW", "HtoWW2Q1L", "HtoWW4Q", "TTBar", "TTBarLep", 
+                "WtoQQ", "ZJetstoNuNu", "ZtoQQ"]
+    all_particle_features = ["part_px", "part_py", "part_pz", "part_energy", "part_deta", "part_dphi", "part_d0val", "part_d0err", "part_dzval",
+                             "part_dzerr", "part_charge", "part_isChargedHadron", "part_isNeutralHadron", "part_isPhoton", "part_isElectron", "part_isMuon"]
+    all_jet_features = ["jet_pt", "jet_eta", "jet_phi", "jet_energy", "jet_nparticles", "jet_sdmass", "jet_tau1", "jet_tau2", "jet_tau3", "jet_tau4"]
     splits = ["train", "valid", "test", "all"]
 
     def __init__(
@@ -55,11 +41,6 @@ class JetClass:
         data_dir: str = "./",
         particle_features: List[str] = all_particle_features,
         jet_features: List[str] = all_jet_features,
-        particle_normalisation: Optional[NormaliseABC] = None,
-        jet_normalisation: Optional[NormaliseABC] = None,
-        particle_transform: Optional[Callable] = None,
-        jet_transform: Optional[Callable] = None,
-        num_particles: int = 30,
         split: str = "train",
         split_fraction: List[float] = [0.7, 0.15, 0.15],
         seed: int = 42,
@@ -68,134 +49,63 @@ class JetClass:
             jet_type,
             data_dir,
             particle_features,
-            jet_features,
-            num_particles,
-            split,
-            split_fraction,
-            seed,
+            jet_features
         )
 
         super().__init__(
             data_dir=data_dir,
             particle_features=particle_features,
             jet_features=jet_features,
-            particle_normalisation=particle_normalisation,
-            jet_normalisation=jet_normalisation,
-            particle_transform=particle_transform,
-            jet_transform=jet_transform,
-            num_particles=num_particles,
         )
-
-        self.jet_type = jet_type
         self.split = split
         self.split_fraction = split_fraction
 
     @classmethod
-    def getData(
-        cls: JetDataset,
-        jet_type: Union[str, Set[str]] = "all",
-        data_dir: str = "./",
-        particle_features: List[str] = all_particle_features,
-        jet_features: List[str] = all_jet_features,
-        num_particles: int = 30,
-        split: str = "all",
-        split_fraction: List[float] = [0.7, 0.15, 0.15],
-        seed: int = 42,
-    ) -> Tuple[Optional[np.ndarray], Optional[np.ndarray]]:
-        """
-        Downloads, if needed, and loads and returns JetNet data.
-        Args:
-            jet_type (Union[str, Set[str]], optional): individual type or set of types out of
-                'g' (gluon), 't' (top quarks), 'q' (light quarks), 'w' (W bosons), or 'z' (Z bosons).
-                "all" will get all types. Defaults to "all".
-            data_dir (str, optional): directory in which data is (to be) stored. Defaults to "./".
-            particle_features (List[str], optional): list of particle features to retrieve. If empty
-                or None, gets no particle features. Defaults to
-                ``["etarel", "phirel", "ptrel", "mask"]``.
-            jet_features (List[str], optional): list of jet features to retrieve.  If empty or None,
-                gets no jet features. Defaults to
-                ``["type", "pt", "eta", "mass", "num_particles"]``.
-            num_particles (int, optional): number of particles to retain per jet, max of 150.
-                Defaults to 30.
-            split (str, optional): dataset split, out of {"train", "valid", "test", "all"}. Defaults
-                to "train".
-            split_fraction (List[float], optional): splitting fraction of training, validation,
-                testing data respectively. Defaults to [0.7, 0.15, 0.15].
-            seed (int, optional): PyTorch manual seed - important to use the same seed for all
-                dataset splittings. Defaults to 42.
-        Returns:
-            Tuple[Optional[np.ndarray], Optional[np.ndarray]]: particle data, jet data
-        """
-        assert (
-            num_particles <= cls.max_num_particles
-        ), f"num_particles {num_particles} exceeds max number of particles in the dataset {cls.max_num_particles}"
-        jet_type = checkConvertElements(jet_type, cls.jet_types, ntype="jet type")
-        particle_features, jet_features = checkStrToList(particle_features, jet_features)
-        use_particle_features, use_jet_features = checkListNotEmpty(particle_features, jet_features)
-
-        import h5py
-
-        # Use JetNet150 if ``num_particles`` > 30
-        use_150 = num_particles > 30
-
-        particle_data = []
-        jet_data = []
-
-        for j in jet_type:
-            dname = f"{j}{'150' if use_150 else ''}"
-
-            hdf5_file = checkDownloadZenodoDataset(
-                data_dir,
-                dataset_name=dname,
-                record_id=cls._zenodo_record_ids["150" if use_150 else "30"],
-                key=f"{dname}.hdf5",
-            )
-
-            with h5py.File(hdf5_file, "r") as f:
-                pf = (
-                    np.array(f["particle_features"])[:, :num_particles]
-                    if use_particle_features
-                    else None
-                )
-                jf = np.array(f["jet_features"]) if use_jet_features else None
-
-            if use_particle_features:
-                # reorder if needed
-                pf = getOrderedFeatures(pf, particle_features, cls.all_particle_features)
-
-            if use_jet_features:
-                # add class index as first jet feature
-                class_index = cls.jet_types.index(j)
-                jf = np.concatenate(
-                    (
-                        np.full([len(jf), 1], class_index),
-                        jf[:, :3],
-                        # max particles should be num particles
-                        np.minimum(jf[:, 3:], num_particles),
-                    ),
-                    axis=1,
-                )
-                # reorder if needed
-                jf = getOrderedFeatures(jf, jet_features, cls.all_jet_features)
-
-            particle_data.append(pf)
-            jet_data.append(jf)
-
-        particle_data = np.concatenate(particle_data, axis=0) if use_particle_features else None
-        jet_data = np.concatenate(jet_data, axis=0) if use_jet_features else None
-
-        length = len(firstNotNoneElement(particle_data, jet_data))
-
-        # shuffling and splitting into training and test
-        lcut, rcut = getSplitting(length, split, cls.splits, split_fraction)
-
-        np.random.seed(seed)
-        randperm = np.random.permutation(length)
-
-        if use_particle_features:
-            particle_data = particle_data[randperm][lcut:rcut]
-
-        if use_jet_features:
-            jet_data = jet_data[randperm][lcut:rcut]
-
-        return particle_data, jet_data
+    def getData(self,jet_type, data_dir, particle_features, jet_features):
+        dataset_name = "JetClass Validation Set"
+        file_download_name = "Val_5M"
+        key = "JetClass_Pythia_val_5M.tar"
+        record_id = 6619768
+        jet_matrix = np.zeros((1, 100000))
+        particle_matrix = np.zeros((1, 136))
+        file_path = checkDownloadZenodoDataset(data_dir, dataset_name, record_id, key, file_download_name)
+        print("Processing Data: ...")
+        for jet_file in os.listdir(file_path):
+            f = os.path.join(file_path, jet_file)
+            for jet in jet_type:
+                if jet in f:
+                    open_file = uproot.open(f)
+                    branch = open_file['tree']
+                    for i in branch.keys():
+                        for feature in jet_features:
+                            if feature in i:
+                                arr = branch[i].array()
+                                arr = np.array(arr)
+                                jet_matrix = np.vstack([jet_matrix, arr])
+                        for particle in particle_features:
+                            if particle in i:
+                                arr_awk = branch[i].array()
+                                awk_list = list(arr_awk)
+                                zero_pad_arr = zero_padding(awk_list)
+                                length_curr = findMaxLengthList(zero_pad_arr)
+                                length_matrix = findMaxLengthList(particle_matrix)
+                                zeros = np.zeros(100001)
+                                if (length_curr > length_matrix) :
+                                    zeros = np.zeros(100001)
+                                    diff = length_curr - length_matrix
+                                    for i in range(diff):
+                                        particle_matrix = np.column_stack((particle_matrix,zeros))
+                                elif (length_curr < length_matrix):
+                                    zeros = np.zeros(100000)
+                                    diff = length_matrix - length_curr
+                                    for i in range(diff):
+                                        zero_pad_arr = np.column_stack((zero_pad_arr,zeros))
+                                particle_matrix = np.vstack([particle_matrix, zero_pad_arr])
+                                updated_particle_matrix = np.delete(particle_matrix, 0 , axis = 0)   
+                
+        updated_jet_matrix = np.delete(jet_matrix, 0 , axis = 0)                
+        dim1 = updated_jet_matrix.shape[0]
+        dim2 = updated_jet_matrix.shape[1]
+        dim_res = dim1/len(jet_features)
+        dim = int(dim_res * dim2)
+        return updated_jet_matrix.reshape(dim,len(jet_features)) , updated_particle_matrix
